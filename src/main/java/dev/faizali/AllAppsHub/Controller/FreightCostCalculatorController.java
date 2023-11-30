e1e97dc3e7530997222fe21b0ecf23e1daf4c92d
Faizali Muhammadzoda <faizaliyusuf@gmail.com>
2023-11-30 20:02:03 +0400

    Updated the UI design and fixed some bugs

--- src/main/java/dev/faizali/AllAppsHub/Controller/FreightCostCalculatorController.java
+++ src/main/java/dev/faizali/AllAppsHub/Controller/FreightCostCalculatorController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FreightCostCalculatorController {
 
-    @GetMapping("freightcostcalculator")
-    public String getFreightCostCalculator(){
-        return "calculatefreightcost";
+    @GetMapping("alib/cfc")
+    public String getFreightCostCalculator2(){
+        return "calculatefreightcostv2";
     }
 }
