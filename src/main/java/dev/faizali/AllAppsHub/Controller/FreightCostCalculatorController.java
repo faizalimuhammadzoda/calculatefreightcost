9cab311dcc300ebe1ac4abb541f4205aed23146b
Faizali Muhammadzoda <faizaliyusuf@gmail.com>
2023-12-04 18:20:34 +0400

    Adding a home page

--- src/main/java/dev/faizali/AllAppsHub/Controller/FreightCostCalculatorController.java
+++ src/main/java/dev/faizali/AllAppsHub/Controller/FreightCostCalculatorController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FreightCostCalculatorController {
 
+    @GetMapping("/")
+    public String showHomePage(){
+        return "home";
+    }
+
     @GetMapping("alib/cfc")
     public String getFreightCostCalculator2(){
         return "calculatefreightcostv2";
